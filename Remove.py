1c7decd7883133dbc8fa8c1abf747d809a35c881
CYBER KING SQUAD <105499497+CYBERKINGSQUAD@users.noreply.github.com>
2022-10-07 12:13:08 +0600

    Update Remove.py

--- Remove.py
+++ Remove.py
@@ -23,21 +23,28 @@ print ("""\033[1;32;40m
 \n""")
 try:
     File = raw_input("\n\033[91mEnter Text File \033[97m:~# \033[97m")
+    outfile = raw_input("\n\033[91mEnter Outfile File Name \033[97m:~# \033[97m")
     word = raw_input("\n\033[91mEnter Remove Text\033[97m:~# \033[97m")
 
     with open(File, 'r') as fr:
         lines = fr.readlines()
-        with open(File , 'w') as fw:
+        with open(File, 'w') as fw:
             for line in lines:
 
                 if line.find(word) == -1:
 
                     fw.write(line)
-    
+    lines_seen = set() 
+    outfile = open((outfile) + ".txt", "w")
+    for line in open(File, "r"):
+         if line not in lines_seen: 
+            outfile.write(line)
+            lines_seen.add(line)
+    outfile.close()
 
     print ("""\033[1;32;40mDeleted[ Check Your Output File Dear ] [ Stay_With_CKS ]\n""")
 
 except:
     
-    
+    print("")
     print ("""\033[1;32;91m[ something error!! ] [ Stay_With_CKS ]\n""")
